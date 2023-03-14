8f0773f427832c831de8d7761d57d621efde304d
gag1005 <gag1005@alu.ubu.es>
2023-03-14 09:27:40 +0100

    donete?

--- practica2/iris/knn.py
+++ practica2/iris/knn.py
@@ -1,6 +1,7 @@
 # from myknn import euclideanDistanceVec
 import numpy as np
 import pandas as pd
+from collections import Counter
 
 def knn_distancias(xTrain,xTest,k):
     """
@@ -18,15 +19,19 @@ def knn_distancias(xTrain,xTest,k):
 
     #returning the top-k closest distances.
 
+    # Se aplica el método knn_distancias_single a cada elemento de xTest
     result = np.apply_along_axis(knn_distancias_single, 1, xTest, xTrain, k)
-
     return result[:,:,1], result[:,:,0]
 
 def knn_distancias_single(single, xTrain, k):
+    # Se obtiene el número de elementos
     size = xTrain.shape[0]
+    # Se calculan las distancias a cada punto
     distances = np.apply_along_axis(euclideanDistanceVec, 1, xTrain, single)
+    # Se une el array de distancias a un array de índices
     distances = np.stack((np.arange(size), distances)).transpose()
-    distances = distances[distances[:, 1].argsort()][:k] # Esto ordena el array según la segunda columna (la distancia)
+    # Se ordena la matriz según la columna de distancias y se escojen los k primeros
+    distances = distances[distances[:, 1].argsort()][:k]
     return distances
 
 
@@ -42,14 +47,20 @@ def knn_predecir(xTrain,yTrain,xTest,k=3):
     predicciones = etiquetas predichas, es decir, preds(i) es la etiqueta predicha de xTest(i,:)
 
     """
+    # Se convierte el DataFrame yTrain a un array de numpy
     yTrain = yTrain.to_numpy()
+    # Se calculan las distancias e índices de los puntos mas cercanos a cada punto de xTest
+    # las distancias son innecesarias
     dists, inds = knn_distancias(xTrain, xTest, k)
+    # Se convierte una función de un único elemento en vectorial (funciona con varios elementos)
     getTag = np.vectorize(lambda x: yTrain[int(x)][0])
+    # Se aplica la función anterior al array inds para obtener las etiquetas que corresponden a cada índice
     tags = getTag(inds)
-    predicts = np.apply_along_axis(lambda x: pd.DataFrame(x).mode()[0][0], 1, tags)
-    return predicts.transpose()
+    # Se obtiene por cada elemento la etiqueta mas común
+    predicts = np.apply_along_axis(lambda x: Counter(x).most_common()[0], 1, tags)
+    return predicts[:, 0].transpose()
 
-    # return np.apply_along_axis(lambda x: pd.DataFrame(x).mode()[0][0], 1, np.vectorize(lambda x: yTrain.to_numpy()[int(x)][0])(knn_distancias(xTrain, xTest, k)[1])).transpose()
+    # return np.apply_along_axis(lambda x: Counter(x).most_common()[0], 1, np.vectorize(lambda x: yTrain.to_numpy()[int(x)][0])(knn_distancias(xTrain, xTest, k)[1]))[:, 0].transpose()
 
 def knn_precision(yTest, predictions):
     """
