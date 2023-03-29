89be5698021835255e84b6f34c6504f29a2f180e
Guilleag01 <guilleag01@gmail.com>
2023-03-29 12:37:39 +0200

    No se

--- practica3/linReg.py
+++ practica3/linReg.py
@@ -7,26 +7,30 @@ class LinRegClassifier:
         self.xTrain = xTrain.to_numpy()
         self.yTrain = yTrain.to_numpy()
         #self.coefs = np.random.rand(xTrain.shape[1] + 1)
-        self.coefs = np.zeros(xTrain.shape[1] + 1)
+        self.coefs = np.zeros(xTrain.shape[1] + 1) + 1
         self.numIter = numIter
         self.lRate = lRate
         self.n = xTrain.shape[0]
 
     def train(self):
-        for i in range(10):
-            print(np.stack((self.coefs, self.dmse())))
-            self.coefs = np.apply_along_axis(lambda x: x[0] - self.lRate * x[1], 0, np.stack((self.coefs, self.dmse())))
+        for i in range(5):
+            # print(np.array(self.coefs[:-1]))
+            coefVars = np.concatenate((np.apply_along_axis(self.dmse, 0, np.array([self.coefs[:-1]])), [self.dmse(1)]))
+            print(coefVars)
+            self.coefs = np.apply_along_axis(lambda x: x[0] - self.lRate * x[1], 0, np.stack((self.coefs, coefVars)))
+            print(self.coefs)
+            # print(np.stack((self.coefs, self.dmse())))
+            # self.coefs = np.apply_along_axis(lambda x: x[0] - self.lRate * x[1], 0, np.stack((self.coefs, self.dmse())))
         
-        print(self.coefs)
 
 
     # def mse(self) -> float:
     #     # TODO: Probar con np.square a ver si es mas rápido
     #     return (1 / self.xTrain.shape[0]) * np.sum(np.apply_along_axis(lambda x: (self.getValue(x[:-1]) - x[-1]) ** 2, 1, np.concatenate((self.xTrain, self.yTrain), axis=1)))
     
-    def dmse(self) -> np.ndarray:
+    def dmse(self, x) -> np.ndarray:
         difs = (self.yTrain.transpose() - np.apply_along_axis(self.getValue, 1, self.xTrain))[0]
-        coefVar = np.apply_along_axis(lambda x: ((-2) / self.n) * np.sum(difs), 0, np.array([self.coefs]))
+        coefVar = ((-2) / self.n) * np.sum(x * difs)
         return coefVar
     
     def getValue(self, point:np.ndarray) -> float:
