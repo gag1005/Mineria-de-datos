c878e2ff148473ae44a4b6e0df059e1f392b0b4b
Guilleag01 <guilleag01@gmail.com>
2023-04-02 12:15:48 +0200

    Creo que lo he hecho bien ahora, pero no funciona

--- practica3/linReg.py
+++ practica3/linReg.py
@@ -3,38 +3,25 @@ import pandas as pd
 
 class LinRegClassifier:
 
-    def __init__(self, xTrain:pd.DataFrame, yTrain:pd.DataFrame, numIter:int=1000, lRate:float=0.001) -> None:
+    def __init__(self, xTrain:pd.DataFrame, yTrain:pd.DataFrame, numIter, lRate) -> None:
         self.xTrain = xTrain.to_numpy()
         self.yTrain = yTrain.to_numpy()
-        #self.coefs = np.random.rand(xTrain.shape[1] + 1)
-        self.coefs = np.zeros(xTrain.shape[1] + 1) + 1
+        self.coefs = np.zeros(xTrain.shape[1] + 1)
         self.numIter = numIter
         self.lRate = lRate
         self.n = xTrain.shape[0]
 
     def train(self):
         for i in range(5):
-            # print(np.array(self.coefs[:-1]))
-            coefVars = np.concatenate((np.apply_along_axis(self.dmse, 0, np.array([self.coefs[:-1]])), [self.dmse(1)]))
+            coefVars = np.array([self.dmse(x) for x in range(self.coefs.size)])
             print(coefVars)
-            self.coefs = np.apply_along_axis(lambda x: x[0] - self.lRate * x[1], 0, np.stack((self.coefs, coefVars)))
+            self.coefs = self.coefs - self.lRate * coefVars
             print(self.coefs)
-            # print(np.stack((self.coefs, self.dmse())))
-            # self.coefs = np.apply_along_axis(lambda x: x[0] - self.lRate * x[1], 0, np.stack((self.coefs, self.dmse())))
-        
 
-
-    # def mse(self) -> float:
-    #     # TODO: Probar con np.square a ver si es mas rápido
-    #     return (1 / self.xTrain.shape[0]) * np.sum(np.apply_along_axis(lambda x: (self.getValue(x[:-1]) - x[-1]) ** 2, 1, np.concatenate((self.xTrain, self.yTrain), axis=1)))
-    
-    def dmse(self, x) -> np.ndarray:
-        difs = (self.yTrain.transpose() - np.apply_along_axis(self.getValue, 1, self.xTrain))[0]
-        coefVar = ((-2) / self.n) * np.sum(x * difs)
+    def dmse(self, coefnum:int) -> float:
+        difs = np.apply_along_axis(lambda x: np.append(x[:-1], 1)[coefnum] * (self.predict(x[:-1]) - x[-1]), 1, np.concatenate((self.xTrain, self.yTrain), axis=1))
+        coefVar = -2 * np.sum(difs) / self.n
         return coefVar
     
-    def getValue(self, point:np.ndarray) -> float:
+    def predict(self, point:np.ndarray) -> float:
         return np.sum(self.coefs[:-1] * point) + self.coefs[-1]
-        
-    
-
