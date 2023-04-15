331de31ecbbc847b54c608e0d6a5e43972e7fd78
Guilleag01 <guilleag01@gmail.com>
2023-04-15 22:35:47 +0200

    multiprocessing

--- practica3/linReg.py
+++ practica3/linReg.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+from multiprocessing import Pool
+import multiprocessing
+from functools import partial
 
 class LinRegClassifier:
 
@@ -15,13 +18,22 @@ class LinRegClassifier:
         for i in range(5):
             coefVars = np.array([self.dmse(x) for x in range(self.coefs.size)])
             print(coefVars)
-            self.coefs = self.coefs - self.lRate * coefVars
+            self.coefs -= self.lRate * coefVars
             print(self.coefs)
+            print("")
 
     def dmse(self, coefnum:int) -> float:
-        difs = np.apply_along_axis(lambda x: np.append(x[:-1], 1)[coefnum] * (self.predict(x[:-1]) - x[-1]), 1, np.concatenate((self.xTrain, self.yTrain), axis=1))
+        pool = Pool()
+
+        # func = lambda x: np.append(x[:-1], 1)[coefnum] * (self.predict(x[:-1]) - x[-1])
+
+        difs = np.array(pool.map(partial(self.func, coefnum), np.concatenate((self.xTrain, self.yTrain), axis=1), chunksize=int(self.n / (multiprocessing.cpu_count() * 2))))
+        # difs = np.apply_along_axis(lambda x: np.append(x[:-1], 1)[coefnum] * (self.predict(x[:-1]) - x[-1]), 1, np.concatenate((self.xTrain, self.yTrain), axis=1))
         coefVar = -2 * np.sum(difs) / self.n
         return coefVar
     
     def predict(self, point:np.ndarray) -> float:
         return np.sum(self.coefs[:-1] * point) + self.coefs[-1]
+
+    def func(self, coefnum, x):
+        return np.append(x[:-1], 1)[coefnum] * (self.predict(x[:-1]) - x[-1])
