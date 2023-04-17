810ce80ec7e980cc25da4ea733374ebb1b331e4f
Guilleag01 <guilleag01@gmail.com>
2023-04-17 21:35:48 +0200

    util

--- practica3/linReg.py
+++ practica3/linReg.py
@@ -21,39 +21,45 @@ class LinRegClassifier:
         self.numIter = numIter
         self.lRate = lRate
         self.n = xTrain.shape[0]
-        self.d:list= []
+        self.mseHistory: np.ndarray = np.empty(self.numIter)
+
 
 
     def train(self):
+        it = 0
+        maxMSE = 1
+        
+        pool = Pool(processes=multiprocessing.cpu_count())
         for i in range(self.numIter):
-        # coefVars = [-1 for i in self.coefs]
-        # while(-1 * np.max(coefVars) > (10**(-10))):
-            # coefVars = np.array([self.dmse(x) for x in range(self.coefs.size)])
             nums = np.array([np.arange(self.coefs.size)])
-            coefVars = np.apply_along_axis(self.dmse, 0, nums)
-            # msevars = np.apply_along_axis(self.mse, 0, nums)
-            print(coefVars)
-            # print(msevars)
+            coefVars = np.apply_along_axis(self.dmse, 0, nums, pool)
             self.coefs -= self.lRate * coefVars
-            print(self.coefs)
-            print("")
 
-    def dmse(self, coefnum:int) -> float:
-        difs = self.difs(coefnum)
+            msevars = np.apply_along_axis(self.mse, 0, nums)
+            maxMSE = np.max(msevars)
+            self.mseHistory[i] = maxMSE
+
+            print("Iter: " + str(it) + " max MSE: " + str(maxMSE))
+            it += 1
+        pool.close()
+
+
+    def dmse(self, coefnum:int, pool) -> float:
+        difs = self.difs(coefnum, pool)
         coefVar = np.sum(difs) / self.n
         # if coefnum == 1:
             # self.d.append(coefVar)
         return coefVar
 
     
-    def difs(self, coefnum: int):
-        pool = Pool(processes=multiprocessing.cpu_count())
+    def difs(self, coefnum: int, pool):
+        # pool = Pool(processes=multiprocessing.cpu_count())
+        
         csize = 1
         csize, extra = divmod(self.n, multiprocessing.cpu_count())
         csize += not not extra
-        difs = np.array(list(pool.imap(partial(self.dmseFunc, coefnum), np.concatenate((self.xTrain, self.yTrain), axis=1), chunksize=csize)))
-        pool.close()
-        # print(np.concatenate((self.xTrain, self.yTrain), axis=1))
+        difs = np.array(list(pool.map(partial(self.dmseFunc, coefnum), np.concatenate((self.xTrain, self.yTrain), axis=1), chunksize=csize)))
+        # pool.close()
         # difs = np.apply_along_axis(lambda x: self.dmseFunc(coefnum, x), 1, np.concatenate((self.xTrain, self.yTrain), axis=1))
 
         return difs
@@ -64,31 +70,10 @@ class LinRegClassifier:
     def dmseFunc(self, coefnum, x):
         # print("-> " + str(np.append(x[:-1], 1)[coefnum]))
         return np.append(x[:-1], 1)[coefnum] * (self.predict(x[:-1]) - x[-1])
-    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
     def predict2d(self, value, coefnum):
         return self.coefs[coefnum] * value + self.coefs[-1]
     
-        # difs = np.apply_along_axis(lambda x: np.append(x[:-1], 1)[coefnum] * (self.predict(x[:-1]) - x[-1]), 1, np.concatenate((self.xTrain, self.yTrain), axis=1))
-    
-
-
-
-
     def mse(self, coefnum:int) -> float:
         difs = self.difsmse(coefnum)
         coefVar = np.sum(difs) / self.n
---
+++ practica3/util.py
@@ -0,0 +1,25 @@
+import math
+import numpy as np
+import pandas as pd
+
+def coordsToDistance(long1: float, lat1: float, long2: float, lat2: float):
+    # Radio de la tierra aprox
+    R = 6367000
+    # Grados a radianes
+    degToRad = math.pi / 180
+
+    distLong = (long2 - long1) * degToRad
+    distLat = (lat1 - lat2) * degToRad
+    
+    a = (math.sin(distLat / 2) ** 2) + (math.cos(lat1 * degToRad) * math.cos(lat2 * degToRad) * (math.sin(distLong / 2) ** 2))
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    d = R * c
+    return d
+
+def transformDataframeCoordsIntoDistance(df: pd.DataFrame, cols: list[str], newColName: str):
+    # ['pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude']
+    coords:np.ndarray = df[cols].to_numpy()
+    distances = np.apply_along_axis(lambda x: coordsToDistance(x[0], x[1], x[2], x[3]), 1, coords)
+    df.insert(2, newColName, pd.Series(distances))
+
+
