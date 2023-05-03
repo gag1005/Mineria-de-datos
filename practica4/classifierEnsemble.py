f5bd1ad1870bfda32c309b8b37e01eb274a6f0de
gag1005 <gag1005@alu.ubu.es>
2023-05-03 17:14:09 +0200

    lul

--- practica4/classifierEnsemble.py
+++ practica4/classifierEnsemble.py
@@ -1,12 +1,22 @@
 import pandas as pd
 from sklearn.base import ClassifierMixin
+import numpy as np
 
 class ClassifierEnsemble(ClassifierMixin):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, classifierList: list[tuple[str, ClassifierMixin]], weights: list[float] = []) -> None:
+        self.classifierList = classifierList
+        self.weights = [1 for i in range(len(classifierList))] if weights == [] else self.weights
+        self.splits: list[pd.DataFrame]
+        self.n: int
 
-    def fit(self):
-        pass
+    def fit(self, xx: pd.DataFrame, yy: pd.DataFrame):
+        x = xx.to_numpy()
+        y = yy.to_numpy()
+        self.n = yy.size
+        self.splitData(xx, yy)
 
     def predict(self):
         pass
+
+    def splitData(self, x: pd.DataFrame, y: pd.DataFrame):
+        self.splits = [pd.concat((x, y)).sample(int(self.n / len(self.classifierList))) for i in range(len(self.classifierList))]
