4976c67b1e2416d47799351098505ff59709c9c3
gag1005 <gag1005@alu.ubu.es>
2023-05-07 07:44:48 +0200

    sigh...

--- practica4/classifierEnsemble.py
+++ practica4/classifierEnsemble.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from sklearn.base import ClassifierMixin
 import numpy as np
+from typing import Union
 
 class ClassifierEnsemble(ClassifierMixin):
     def __init__(self, classifierList: list[tuple[str, ClassifierMixin]], weights: list[float] = []) -> None:
         self.classifierList = classifierList
         self.weights = [1 for i in range(len(classifierList))] if weights == [] else self.weights
-        self.splits: list[pd.DataFrame]
+        self.splits: list[Union[np.ndarray, tuple[np.ndarray, np.ndarray]]]
         self.n: int
 
     def fit(self, xx: pd.DataFrame, yy: pd.DataFrame):
@@ -14,9 +15,13 @@ class ClassifierEnsemble(ClassifierMixin):
         y = yy.to_numpy()
         self.n = yy.size
         self.splitData(xx, yy)
+        for c, d in zip(self.classifierList, self.splits):
+            c[1].fit(d[0], d[1])
+        # for c, d in zip(self.classifierList, self.splits): c[1].fit(d)
 
     def predict(self):
         pass
 
     def splitData(self, x: pd.DataFrame, y: pd.DataFrame):
-        self.splits = [pd.concat((x, y)).sample(int(self.n / len(self.classifierList))) for i in range(len(self.classifierList))]
+        self.splits = [pd.concat((x, y)).sample(int(self.n / len(self.classifierList))).to_numpy() for i in range(len(self.classifierList))]
+        self.splits = [(s[:, :-1], s[:, -1]) for s in self.splits]
